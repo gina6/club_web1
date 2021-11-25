46ac00bf7dea2e0c4d712f3309520b7bff693c1a
Michelle <michelle.egli.13@gmail.com>
2021-11-25 22:13:38 +0100

    Background done with V <3

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,7 @@
 <template>
+<div id="backgroundblack">
   <div id="container">
-    <img src="../assets/About_pagetitle.svg" alt="About" />
+    <img class="About1" src="../assets/About_pagetitle.svg" alt="About" />
     <div class="info">
       <h2>CREATORS</h2>
       <ul>
@@ -15,6 +16,7 @@
       </p>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -25,15 +27,28 @@
 #container {
   display: flex;
   flex-direction: row;
-  margin: 10%;
   padding: 5%;
-  height: 50vh;
+
+  width: 90vw;
+  height: 77vh;
+  /*min-height: 100vh;*/
+  overflow-y: scroll;
+  background: linear-gradient(#00000000, #000000b0);
+  z-index: -1;
+  
+}
+
+.About1{
+  position:relative;
+  top: -11%;
+  left: 7%;
+  padding: 21% 0% 0% 10%;
 }
 
 .info {
   display: flex;
   flex-direction: column;
-  margin: 10% 20%;
+  margin: 25% 18% 25% 15%;
   }
 
 ul {
